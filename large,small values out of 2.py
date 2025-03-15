c0d433affb28fb9fb68e6f7ee38d5a4ac0724df3
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-15 15:42:15 +0530

    Create large,small values out of 2.py

---
+++ large,small values out of 2.py
@@ -0,0 +1,8 @@
+a=int(input("enter num1="))
+b=int(input("enter num2="))
+if(a>b):
+  print("largest number=",a)
+  print("smallest number=",b)
+else:
+  print("largest number=",b)
+  print("smallest number=",a)
