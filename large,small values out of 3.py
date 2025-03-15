dd1e5ff2f6087881545596c0e435e2ae6b2e57ea
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-15 15:56:07 +0530

    Create large,small values out of 3.py

---
+++ large,small values out of 3.py
@@ -0,0 +1,15 @@
+a=int(input("enter num1="))
+b=int(input("enter num2="))
+c=int(input("enter num3="))
+if(a>b and a>c):
+  print("largest=",a)
+elif(b>a and b>c):
+  print("largest=",b)
+else:
+  print("largest=",c)
+if(a<b and a<c):
+  print("smallest=",a)
+elif(b<a and b<c):
+  print("smallest=",b)
+else:
+  print("smallest=",c)
