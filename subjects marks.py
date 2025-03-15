1a2cabeeb970bc58ca66dd85a8e577a05fcbbf15
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-15 14:53:02 +0530

    Create subjects marks.py

---
+++ subjects marks.py
@@ -0,0 +1,7 @@
+a=int(input("enter marks of subject 1="))
+b=int(input("enter marks of subject 2="))
+c=int(input("enter marks of subject 3="))
+d=a+b+c
+e=d/3
+print("total marks=",d)
+print("average marks=",e)
