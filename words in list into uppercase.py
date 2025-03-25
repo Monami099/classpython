3417566369c6b31f48290d66bad86cab2e5c4b13
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-25 15:12:29 +0530

    Update words in list into uppercase.py

--- words in list into uppercase.py
+++ words in list into uppercase.py
@@ -1,3 +1,6 @@
-words_list=["apple","banana","cherry","orange","pear
+words_list=["apple","banana","cherry","orange","pear"]
 uppercase_set={word.upper() for word in words_list}
 print("set of uppercase words:",uppercase_set)
+
+output:
+set of uppercase words: {'CHERRY', 'APPLE', 'ORANGE', 'BANANA', 'PEAR'}
