573d1c3bd3445cfd1d18e0638f98788591f30a96
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-25 15:18:39 +0530

    Update random numbers btw 15,45.py

--- random numbers btw 15,45.py
+++ random numbers btw 15,45.py
@@ -1,7 +1,12 @@
 import random
-random_set={random.randint(15,45)for_in range(10)}
-count_less_than_30=sum(1 for num in random_set if num<30)
-filtered_set={num for num in random_set if num<=35}
-print("original set:",random_set)
+random_numbers=set(random.sample(range(15,45),(10)))
+count_less_than_30=sum(1 for num in random_numbers if num<30)
+filtered_numbers={num for num in random_numbers if num<=35}
+print("random numbers:",random_numbers)
 print("count of numbers less than 30:",count_less_than_30)
-print("set after deleting numbers greater than 35:",filtered_set
+print("numbers after deleting numbers greater than 35:",filtered_numbers)
+
+output:
+random numbers: {32, 33, 39, 16, 20, 21, 22, 23, 26, 29}
+count of numbers less than 30: 7
+numbers after deleting numbers greater than 35: {32, 33, 16, 20, 21, 22, 23, 26, 29}
