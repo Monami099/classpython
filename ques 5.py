f3c3701e0763561fb201c79c1c9db9d9bf4ea1d8
Monami099 <24bch099@spt.pdpu.ac.in>
2025-03-25 15:05:43 +0530

    Create ques 5.py

---
+++ ques 5.py
@@ -0,0 +1,6 @@
+faculty_names=['Alexander','John','Samantha','Christopher','Emily','Rebecca','Jonathan']
+long_names=[name for name in faculty_names if len(name)>8]
+print("names with more than 8 characters:",long_names)
+
+output:
+names with more than 8 characters: ['Alexander', 'Christopher']
