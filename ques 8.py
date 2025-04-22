f0a9cb36491adb29f027da2900d6b2572ff17403
Monami099 <24bch099@spt.pdpu.ac.in>
2025-04-22 15:26:55 +0530

    Create ques 8.py

---
+++ ques 8.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+fact = 1
+for i in range(1, num + 1):
+    fact *= i
+print(f"Factorial of {num} is {fact}")
+
+Output:
+Enter a number: 5
+Factorial of 5 is 120
