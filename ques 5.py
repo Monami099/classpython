aac160f238e5fa2f0f01072b41e3cea8afb82a12
Monami099 <24bch099@spt.pdpu.ac.in>
2025-04-22 15:44:32 +0530

    Create ques 5.py

---
+++ ques 5.py
@@ -0,0 +1,6 @@
+strings = ["hello", "world", "python", "programming", "language"]
+uppercase_strings = [string.upper() for string in strings]
+print("List of strings in uppercase:", uppercase_strings)
+
+Output:
+List of strings in uppercase: ['HELLO', 'WORLD', 'PYTHON', 'PROGRAMMING', 'LANGUAGE']
