de7dee49340d79efbdd6116f11b215d24fd37b56
Monami099 <24bch099@spt.pdpu.ac.in>
2025-04-22 15:13:29 +0530

    Create ques 1.py

---
+++ ques 1.py
@@ -0,0 +1,13 @@
+print("Uppercase Alphabets:")
+for letter in range(ord('A'), ord('Z') + 1):
+    print(chr(letter), end=' ')
+print("\n")
+print("Lowercase Alphabets:")
+for letter in range(ord('a'), ord('z') + 1):
+    print(chr(letter), end=' ')
+
+Output:
+Uppercase Alphabets:
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+Lowercase Alphabets:
+a b c d e f g h i j k l m n o p q r s t u v w x y z 
